33212ea8d751eb8d5360d67dbae64c09a92cd7d8
Asad9340 <asadulimran1999@gmail.com>
2024-04-04 03:54:28 +0600

    added login section only without dynamically

--- src/Pages/Home/MainSection/RightSection.jsx
+++ src/Pages/Home/MainSection/RightSection.jsx
@@ -1,13 +1,32 @@
-
+import { Button } from '@material-tailwind/react';
+import { FaGoogle } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 function RightSection() {
   return (
     <div>
       <h2>Login With</h2>
-      <div>
-        
+      <div className="flex gap-2 flex-col mt-4">
+        <Button
+          size="sm"
+          variant="outlined"
+          color="blue"
+          className="flex items-center gap-2 w-3/4 "
+        >
+          <FaGoogle />
+          Login with Google
+        </Button>
+        <Button
+          size="sm"
+          variant="outlined"
+          color="black"
+          className="flex items-center gap-2 w-3/4"
+        >
+          <FaGithub />
+          Login with Github
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default RightSection
+export default RightSection;
