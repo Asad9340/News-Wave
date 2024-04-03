c40c1ba5e6dad1ca3594ab5b7073a564e4033f0b
Asad9340 <asadulimran1999@gmail.com>
2024-04-04 03:11:53 +0600

    changes in the top bar use moment js for dynamic date and time

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,12 +1,15 @@
+import moment from 'moment';
 import Logo from '../assets/logo.png'
 function TopBar() {
   return (
-    <div className='text-center my-6 md:my-12'>
-      <img className='mx-auto' src={Logo} alt="" />
+    <div className="text-center my-6 md:my-12">
+      <img className="mx-auto" src={Logo} alt="" />
       <h3 className="text-lg text-[#706F6F] mt-4 mb-2">
         Journalism Without Fear or Favour
       </h3>
-      <p className="text-xl text-[#403F3F] font-medium">Sunday, November 27, 2022</p>
+      <p className="text-xl text-[#403F3F] font-medium">
+        {moment().format('dddd, MMMM D , YYYY')}
+      </p>
     </div>
   );
 }
